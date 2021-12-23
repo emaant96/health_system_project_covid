46c4d8dcc895473182064ef7a677fecd3e30923e
OfficialAlienZ <30388366+OfficialAlienZ@users.noreply.github.com>
2021-12-23 23:49:31 +0100

    nuovi ggplot

---
+++ ggplot_theme_Publication.R
@@ -0,0 +1,47 @@
+install.packages("gridExtra")
+library("gridExtra")
+
+theme_Publication <- function(base_size=14, base_family="sans") {
+  library(grid)
+  library(ggthemes)
+  (theme_foundation(base_size=base_size, base_family=base_family)
+    + theme(plot.title = element_text(face = "bold",
+                                      size = rel(1.2), hjust = 0.5),
+            plot.subtitle = element_text(hjust = 0.5),
+            text = element_text(),
+            panel.background = element_rect(color = NA),
+            plot.background = element_rect(color = NA),
+            panel.border = element_rect(color = NA),
+            axis.title = element_text(face = "bold",size = rel(1)),
+            axis.title.y = element_text(angle=90,vjust =2),
+            axis.title.x = element_text(vjust = -0.2),
+            axis.text = element_text(),
+            axis.line.x = element_line(color="black"),
+            axis.line.y = element_line(color="black"),
+            axis.ticks = element_line(),
+            panel.grid.major = element_line(color="#f0f0f0"),
+            panel.grid.minor = element_blank(),
+            legend.key = element_rect(color = NA),
+            legend.position = "bottom",
+            legend.direction = "horizontal",
+            legend.key.size= unit(0.2, "cm"),
+            legend.margin = unit(0, "cm"),
+            legend.title = element_text(face="italic"),
+            plot.margin=unit(c(10,5,5,5),"mm"),
+            strip.background=element_rect(color="#f0f0f0",fill="#f0f0f0"),
+            strip.text = element_text(face="bold")
+  ))
+
+}
+
+scale_fill_Publication <- function(...){
+  library(scales)
+  discrete_scale("fill","Publication",manual_pal(values = c("#386cb0","#fdb462","#7fc97f","#ef3b2c","#662506","#a6cee3","#fb9a99","#984ea3","#ffff33")), ...)
+
+}
+
+scale_color_Publication <- function(...){
+  library(scales)
+  discrete_scale("color","Publication",manual_pal(values = c("#386cb0","#fdb462","#7fc97f","#ef3b2c","#662506","#a6cee3","#fb9a99","#984ea3","#ffff33")), ...)
+
+}
